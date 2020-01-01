b718248fa2c19f6dfd9c70513c6cec72c3596d13
Aaron Azlant <azlant@aarons-mbp.lan>
2020-01-01 17:35:33 -0600

    (fix): removed console.log, added comment

--- src/modules/actions.js
+++ src/modules/actions.js
@@ -1,5 +1,4 @@
 import * as application from './application-old/actions';
-console.log("TCL: application", application)
 
 export default {
     ...application,
---
+++ src/modules/application-rtk/index.js
@@ -0,0 +1,1 @@
+// i.e. https://redux-toolkit.js.org/tutorials/intermediate-tutorial
