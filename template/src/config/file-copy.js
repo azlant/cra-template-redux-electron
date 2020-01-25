1c092259e9b083b2308fa8fdca3be6c24883a306
Aaron Azlant <azlant@aarons-mbp.lan>
2020-01-25 15:24:45 -0600

    (fix): cra-template package.json updated with build script

---
+++ template/src/config/file-copy.js
@@ -0,0 +1,15 @@
+var path = require('path');
+var ncp = require('ncp').ncp;
+
+ncp.limit = 16;
+
+var srcPath = '.'
+var destPath = '../..'
+
+console.log('Copying files...');
+ncp(srcPath, destPath, function (err) {
+  if (err) {
+    return console.error(err);
+  }
+  console.log('Copying files complete.');
+});
