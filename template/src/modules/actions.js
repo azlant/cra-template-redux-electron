394aa03d3143cf9be6bbb9428cee42ec7dbaf8b8
Aaron Azlant <azlant@aarons-mbp.lan>
2020-01-25 16:08:42 -0600

    (fix): cra-template turned on featurefirst

--- template/src/modules/actions.js
+++ template/src/modules/actions.js
@@ -1,6 +1,6 @@
 // import * as application from './application-old/actions';
-// import { applicationOperations } from './application-featurefirst';
-import { applicationSlice } from './application-rtk';
+import { applicationOperations } from './application-featurefirst';
+// import { applicationSlice } from './application-rtk';
 
 // toggle me for application-old
 // export default {
@@ -8,11 +8,11 @@ import { applicationSlice } from './application-rtk';
 // };
 
 // toggle me for application-featurefirst
-// export default {
-//     ...applicationOperations,
-// };
+export default {
+    ...applicationOperations,
+};
 
 // toggle me for application-rtk
-export default {
-    ...applicationSlice.actions
-}
+// export default {
+//     ...applicationSlice.actions
+// }
--- template/src/modules/reducers.js
+++ template/src/modules/reducers.js
@@ -1,13 +1,13 @@
 // import application from './application-old/reducers';
-// import application from './application-featurefirst'
-import { applicationSlice } from './application-rtk';
+import application from './application-featurefirst'
+// import { applicationSlice } from './application-rtk';
 
 // toggle me for application-old or application-featurefirst
-// export default {
-//     application,
-// };
+export default {
+    application,
+};
 
 // toggle me for application-rtk
-export default {
-    application: applicationSlice.reducer,
-};
+// export default {
+//     application: applicationSlice.reducer,
+// };
